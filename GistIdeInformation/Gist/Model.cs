c297c288f50711bb2c98cbd0f718820846479a81
kylewhite <kyle.white@xamarin.com>
2016-04-30 16:46:12 -0400

    add model file

---
+++ GistIdeInformation/Gist/Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace GistIdeInformation
+{
+	class Gist
+	{
+		[JsonProperty(PropertyName = "description")]
+		public string Description { get; set; }
+
+		[JsonProperty(PropertyName = "public")]
+		public bool Public { get; set; }
+
+		[JsonProperty(PropertyName = "files")]
+		public Dictionary<string, GistFile> Files { get; set; }
+	}
+
+	class GistFile
+	{
+		[JsonProperty(PropertyName = "content")]
+		public string Content { get; set; }
+	}
+}
+
