c063b27667c5a1c34f98e4bfea42fbf9f3a3b919
kylewhite <kyle.white@xamarin.com>
2016-04-30 09:55:40 -0400

    Add command to just copy the gist url to clipboard

--- GistIdeInformation/Command.cs
+++ GistIdeInformation/Command.cs
@@ -11,7 +11,8 @@ namespace GistIdeInformation
 {
 	public enum GistCommands
 	{
-		GistIdeInformation
+		GistIdeInformation,
+		CopyGistIdeInformation
 	}
 
 	public class GistCommandHandler : CommandHandler
@@ -20,8 +21,33 @@ namespace GistIdeInformation
 		{
 			try
 			{
-				await CreateGist();
+				var gistUrl = await GistCommandHelper.CreateGist();
+				DesktopService.ShowUrl(gistUrl);
 				IdeApp.Workbench.StatusBar.ShowMessage(Stock.Ok, "Gist created.");	
+			}
+			catch (Exception e)
+			{
+				LoggingService.LogError("Unable to create Gist.", e);
+				IdeApp.Workbench.StatusBar.ShowMessage(Stock.Cancel, "Failed to create gist.");
+			}
+		}
+
+		protected override void Update(CommandInfo info)
+		{
+			base.Update(info);
+			info.Enabled = GistCommandHelper.IsGistAvialable();
+		}
+	}
+
+	public class CopyGistCommandHandler : CommandHandler
+	{
+		protected async override void Run()
+		{
+			try
+			{
+				var gistUrl = await GistCommandHelper.CreateGist();
+				Xwt.Clipboard.SetData(gistUrl);
+				IdeApp.Workbench.StatusBar.ShowMessage(Stock.Ok, "Gist Url copied to clipboard.");
 			}
 			catch (Exception e)
 			{
@@ -36,7 +62,9 @@ namespace GistIdeInformation
 			{
 				var dictionary = new Dictionary<string, string>();
 				dictionary.Add("AboutInformation.txt", SystemInformation.GetTextDescription());
-				dictionary.Add("Ide.log", File.ReadAllText(Path.Combine(UserProfile.Current.LogDir, "Ide.log")));
+
+				var logFile = new FileInfo(Environment.GetEnvironmentVariable("MONODEVELOP_LOG_FILE"));
+				dictionary.Add(logFile.Name, File.ReadAllText(logFile.FullName));
 
 				var client = new GistClient(GistProperties.GitHubUserName, GistProperties.GistPersonalAccessToken);
 				var url = client.CreateGist($"{BrandingService.ApplicationName} {MonoDevelop.BuildInfo.Version} 'about information' and log file. - {DateTime.Now.ToLongDateString()}", dictionary);
@@ -47,10 +75,33 @@ namespace GistIdeInformation
 		protected override void Update(CommandInfo info)
 		{
 			base.Update(info);
+			info.Enabled = GistCommandHelper.IsGistAvialable();
+		}
+	}
 
-			info.Enabled = !string.IsNullOrWhiteSpace(GistProperties.GitHubUserName)
+	class GistCommandHelper
+	{
+		public static async Task<string> CreateGist()
+		{
+			string url = string.Empty;
+			await Task.Run(() =>
+			{
+				var dictionary = new Dictionary<string, string>();
+				dictionary.Add("AboutInformation.txt", SystemInformation.GetTextDescription());
+				dictionary.Add("Ide.log", File.ReadAllText(Path.Combine(UserProfile.Current.LogDir, "Ide.log")));
+
+				var client = new GistClient(GistProperties.GitHubUserName, GistProperties.GistPersonalAccessToken);
+				url = client.CreateGist($"{BrandingService.ApplicationName} {MonoDevelop.BuildInfo.Version} 'about information' and log file. - {DateTime.Now.ToLongDateString()}", dictionary);
+			});
+			return url;
+		}
+
+		public static bool IsGistAvialable()
+		{
+			return !string.IsNullOrWhiteSpace(GistProperties.GitHubUserName)
 				&& !string.IsNullOrWhiteSpace(GistProperties.GistPersonalAccessToken);
 		}
 	}
+
 }
 
