dfae1752b408d71a7fd37fcdbb17fc45494f012e
kylewhite <kyle.white@xamarin.com>
2016-05-11 09:17:51 -0400

    add addin url

--- GistIdeInformation/Properties/AddinInfo.cs
+++ GistIdeInformation/Properties/AddinInfo.cs
@@ -5,11 +5,13 @@ using Mono.Addins.Description;
 [assembly: Addin(
 	"GistIdeInformation",
 	Namespace = "GistIdeInformation",
-	Version = "1.0"
+	Version = "1.0.1"
 )]
 
 [assembly: AddinName("Gist Ide Information")]
 [assembly: AddinCategory("IDE extensions")]
-[assembly: AddinDescription("Adds a Help command to create a Gist with the IDE information and IDE log files. Please see https://github.com/kdubau/GistIdeInformation for README.md and reporting issues.")]
+[assembly: AddinDescription("Adds a Help command to create a Gist with the IDE information and IDE log files. Requires a GitHub personal access token with Gist permission.")]
 [assembly: AddinAuthor("Kyle White")]
+[assembly: AddinUrl("https://github.com/kdubau/GistIdeInformation")]
+
 
