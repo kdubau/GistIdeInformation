63eb792344e108e94197bd80e08b5ae02a81aa05
kylewhite <kyle.white@xamarin.com>
2016-05-13 11:25:26 -0400

    Update description

--- GistIdeInformation/Properties/AddinInfo.cs
+++ GistIdeInformation/Properties/AddinInfo.cs
@@ -10,7 +10,7 @@ using Mono.Addins.Description;
 
 [assembly: AddinName("Gist Ide Information")]
 [assembly: AddinCategory("IDE extensions")]
-[assembly: AddinDescription("Adds a Help command to create a Gist with the IDE information and IDE log files. Requires a GitHub personal access token with Gist permission.")]
+[assembly: AddinDescription("Adds a Help command to create a Gist with the IDE information and IDE log files. Requires a GitHub personal access token with Gist permission. \n\n This is very useful for reporting bugs! https://bugzilla.xamarin.com/")]
 [assembly: AddinAuthor("Kyle White")]
 [assembly: AddinUrl("https://github.com/kdubau/GistIdeInformation")]
 
